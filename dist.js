9f6995cd3e18b8bcb53f5833a27d065adf7a763f
Yifei <yifeizhou.to@gmail.com>
2019-07-22 16:08:17 -0400

    Created cool new component

---
+++ dist.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+module.exports = props => {
+  React.createElement("div", null, "Hi ", props.name, ", how do you add emojis???? Thanks. Sincerely, Yifei Zhou");
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+module.exports = props => {
+    <div>
+      Hi {props.name}, how do you add emojis???? Thanks.
+    </div>
+}
